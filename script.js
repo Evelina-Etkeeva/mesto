24ea55f4ad8508d2eb6e6f2a743a1040ec6b3125
Evelina Etkeeva <evelina.etkeeva@gmail.com>
2021-12-05 09:54:26 +0100

    refactor: comments

--- script.js
+++ script.js
@@ -1,67 +1,56 @@
-//console.log("hi");
-
+//console.log("hi"); Проверка подключения
 let edit = document.querySelector('.edit_button');
-// console.log(edit);
+// console.log(edit); Выбор кнопки редактировать профиль
 let form = document.querySelector('.form');
-//console.log(form);
+//console.log(form); Выбор секции с попапом
 let exit = document.querySelector('.close_button');
-//console.log(exit);
+//console.log(exit); Выбор кнопки закрыть попап
 let page = document.querySelector('.page');
-//console.log(page);
-let formBackground = document.querySelector('.form__background');
-console.log(formBackground);
+//console.log(page); Выбор всей страницы для отклюения скролла
 
-
+//плейсхолдер = данным на странице
 document.querySelector('.form__item_el_name').placeholder = document.querySelector('.profile__name').textContent;
+
 document.querySelector('.form__item_el_about_me').placeholder = document.querySelector('.profile__about_me').textContent;
 
-
+//открытие формы с отключением скролла
 function open_form (event) {
   event.preventDefault();
   form.classList.add('js_form_active');
   page.classList.add('page_no_scroll');
 }
-
+//закрытие формы без сохранения
 function close_form (event) {
   event.preventDefault();
   form.classList.remove('js_form_active');
   page.classList.remove('page_no_scroll');
 }
-
+//вызов по клику
 edit.addEventListener('click', open_form);
-
 exit.addEventListener('click', close_form);
+
 
 // форма имя
 let nameInput = document.querySelector('.form__item_el_name');
-//console.log(nameInput);
 // форма обо мне
 let aboutMeInput = document.querySelector('.form__item_el_about_me');
 //кнопка
 let formElementSaveButton = document.querySelector('.save_button');
-//console.log(formElementSaveButton);
 // имя в профиле
 let myName = document.querySelector('.profile__name');
-//console.log(myName.textContent);
 // обо мне в профиле
-let aboutMe = document.querySelector('.profile__about_me');// Воспользуйтесь инструментом .querySelector
+let aboutMe = document.querySelector('.profile__about_me');
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (event) {
     event.preventDefault();
+    //замена данных нановые
     myName.textContent = nameInput.value;
-    //console.log(myName);
     aboutMe.textContent = aboutMeInput.value;
+    //закрыть окнопопап
     form.classList.remove('js_form_active');
     page.classList.remove('page_no_scroll');
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-
 }
 // Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+// он будет следить за событием “submit” - «отправка» Почему-то на субмит страница не реагирует, а с кликом все работает
 formElementSaveButton.addEventListener('click', formSubmitHandler);
