e9ce4f0aff541422c8e1c0a806fc8d2f0f90784a
Evelina Etkeeva <evelina.etkeeva@gmail.com>
2021-12-20 19:43:31 +0100

    feat: like buttons

--- script/script.js
+++ script/script.js
@@ -1,27 +1,4 @@
-//console.log("hi"); Проверка подключения
-const edit = document.querySelector('.button_type_edit');
-// console.log(edit); Выбор кнопки редактировать профиль
-const popupEditProfile = document.querySelector('.popup_content_edit-profile');
-//кнопка закрыть окно редактирования профиля
-const closeEditProfile = popupEditProfile.querySelector('.button_type_close');
 
-
-
-
-
-//формы в окне изменения информации профиля
-const formEditProfile = document.querySelector('.form_content_edit-profile');
-// форма имя
-const nameInput = document.querySelector('.form__item_el_name');
-// форма обо мне
-const aboutMeInput = document.querySelector('.form__item_el_about-me');
-// имя в профиле
-const myName = document.querySelector('.profile__name');
-// обо мне в профиле
-const aboutMe = document.querySelector('.profile__about-me');
-//кнопка добавить карточки
-const addBtn = document.querySelector('.button_type_add');
-// console.log(addBtn);
 //балванка для карточки
 const elementTemplate = document.querySelector('.elemet-template');
 // console.log(elementTemplate);
@@ -29,17 +6,7 @@ const elementTemplate = document.querySelector('.elemet-template');
 const elementsList = document.querySelector('.elements__list');
 // console.log(elementsList);
 //окно для добавления новых картинок
-const popupAddCard = document.querySelector('.popup_content_add-card');
-//кнопка закрыть окно добавления новых картинок
-const closeAddCard = popupAddCard.querySelector('.button_type_close');
-// форма с добавлением новой картинки
-const formAddCard = document.querySelector('.form_content_add-card');
-console.log(formAddCard);
-console.log(formEditProfile);
-//форма с названием карточки
-const placeNameInput = document.querySelector('.form__item_el_place-name');
-//форма с ссылкой на картинку
-const placeImgInput = document.querySelector('.form__item_el_place-img');
+
 //шесть карточек"из коробки"
 const initialCards = [
   {
@@ -70,7 +37,25 @@ const initialCards = [
 
 
 
+const edit = document.querySelector('.button_type_edit');
+// console.log(edit); Выбор кнопки редактировать профиль
+const popupEditProfile = document.querySelector('.popup_content_edit-profile');
+//кнопка закрыть окно редактирования профиля
+const closeEditProfile = popupEditProfile.querySelector('.button_type_close');
+//формы в окне изменения информации профиля
+const formEditProfile = document.querySelector('.form_content_edit-profile');
+// форма имя
+const nameInput = document.querySelector('.form__item_el_name');
+// форма обо мне
+const aboutMeInput = document.querySelector('.form__item_el_about-me');
+// имя в профиле
+const myName = document.querySelector('.profile__name');
+// обо мне в профиле
+const aboutMe = document.querySelector('.profile__about-me');
+
+
 //открытие формы редактирования профиля
+
 function openPopupEditProfile () {
   nameInput.value = myName.textContent;
   aboutMeInput.value = aboutMe.textContent;
@@ -104,25 +89,37 @@ formEditProfile.addEventListener('submit', formSubmitHandler);
 
 
 // шесть карточек "из коробки"
-  function defaultLayout(list) {
 
-    // console.log(newElement);
-    // console.log(initialCards[0].name);
+  function defaultLayout(list) {
     for(let i=0; i<list.length; i=i+1){
-      const newElement = elementTemplate.content.cloneNode(true).querySelector('.element');
-      newElement.querySelector('.element__title').textContent = list[i].name;
-      newElement.querySelector('.element__image').src = list[i].link;
-      elementsList.append(newElement);
+      const defaultElement = elementTemplate.content.cloneNode(true).querySelector('.element');
+      defaultElement.querySelector('.element__title').textContent = list[i].name;
+      defaultElement.querySelector('.element__image').src = list[i].link;
+      elementsList.append(defaultElement);
+      const defaultLikeButton = defaultElement.querySelector('.button_type_like');// лайки для элементов по умолчанию
+      defaultLikeButton.addEventListener('click', function(){
+        defaultLikeButton.classList.toggle('active');
+        });
+
     }
   }
 
-defaultLayout(initialCards);
 
+defaultLayout(initialCards);
 //открытие формы добавления картинки
+const popupAddCard = document.querySelector('.popup_content_add-card');//попап для добавления новой карточки
+const closeAddCard = popupAddCard.querySelector('.button_type_close');//кнопка закрыть окно добавления новых картинок
+const formAddCard = document.querySelector('.form_content_add-card');// форма с добавлением новой картинки
+const placeNameInput = document.querySelector('.form__item_el_place-name');//форма с названием карточки
+const placeImgInput = document.querySelector('.form__item_el_place-img');//форма с ссылкой на картинку
+const addBtn = document.querySelector('.button_type_add'); //кнопка добавить карточки
+
 function openPopupAddCard () {
   popupAddCard.classList.add('popup_active');
 }
+
 //закрытие формы без сохранения
+
 function closePopupAddCard () {
   popupAddCard.classList.remove('popup_active');
 }
@@ -130,21 +127,31 @@ function closePopupAddCard () {
 addBtn.addEventListener('click', openPopupAddCard);
 closeAddCard.addEventListener('click', closePopupAddCard);
 
-//добавление новой картинки
+
 function addPlaceSubmitHandler (event) {
   event.preventDefault();
   //замена данных на новые
-  console.log('test');
   const newElement = elementTemplate.content.cloneNode(true).querySelector('.element');
   newElement.querySelector('.element__title').textContent  = placeNameInput.value;
   newElement.querySelector('.element__image').src = placeImgInput.value;
+  placeNameInput.value ='';
+  placeImgInput.value = '';
   elementsList.prepend(newElement);
+  const newLikeButton = newElement.querySelector('.button_type_like');
+
+  newLikeButton.addEventListener('click', function(){
+      newLikeButton.classList.toggle('active');
+      });
+
   //закрыть окнопопап
   closePopupAddCard();
+
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка» Почему-то на субмит страница не реагирует, а с кликом все работает
+
 formAddCard.addEventListener('submit', addPlaceSubmitHandler);
+
+
+
 
 
 
